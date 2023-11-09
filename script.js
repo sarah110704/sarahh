44328bbb42cb0f74749a1c86b54652f3af4394e6
sarah110704 <sarahyohana439@gmail.com>
2023-11-10 00:21:15 +0700

    comit 1

---
+++ script.js
@@ -0,0 +1,7 @@
+let state = 0;
+function change(){
+  state++;
+  if (state === 2) location.reload();
+    const semua = document.getElementById('semua').innerHTML = "Anda bisa berlangganan cuku dengan membayar 500k di BelajarYUK";
+    const gambar = document.getElementById('gambar').src = "img/logo2.png";
+  } 
